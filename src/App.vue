93f639382ce329a334403034e270247151b5acd1
geldun98 <geldun98@gmail.com>
2022-03-02 08:58:48 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <Header></Header>
+    <Todos></Todos>
+  </div>
+</template>
+
+<script>
+import Header from './components/Header.vue'
+import Todos from './components/Todos.vue'
+export default {
+name:'App',
+components:{Header,Todos}
+}
+</script>
+
+<style>
+#app{
+   
+}
+</style>
---
+++ src/components/Todos.vue
@@ -0,0 +1,35 @@
+<template>
+    <TodoItem v-for="todo in todos" v-bind:key="todo.id" v-bind:todoProps="todo"/>
+</template>
+
+<script>
+import {ref} from 'vue'
+import TodoItem from './TodoItem.vue'
+export default {
+ name:'Todos-name',
+ components:{TodoItem},
+ setup(){
+     const todos=ref([{
+         id:1,
+         title:'Viec 1',
+         completed:false
+     },{
+         id:2,
+         title:'Viec 2',
+         completed:true
+     },{
+         id:3,
+         title:'Viec 3',
+         completed:true
+     }
+     ])
+     return {
+         todos
+     }
+ }
+}
+</script>
+
+<style>
+
+</style>
